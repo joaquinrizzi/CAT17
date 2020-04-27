cdf9f2688b3ac581d129e902a62c9809ed8ebf6b
Soto <Joaquin.Soto@sabre.com>
2020-04-27 13:17:51 +0200

    changing listener

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+app.listen(port, function() {
 
     console.log(`Starting server at ${port}`);
 
