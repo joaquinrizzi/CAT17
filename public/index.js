6e172a45c5d8c1cc95b60716e36607f3d81b8c50
Soto <joaquin.soto@sabre.com>
2020-04-27 12:14:47 +0200

    first commit of CAT17 project to find rules

---
+++ public/index.js
@@ -0,0 +1,135 @@
+const btnSubmit = document.getElementById('btnSubmit');
+const btnFind = document.getElementById('btnFind');
+
+function sendData() {
+
+    if ('geolocation' in navigator) {
+
+        navigator.geolocation.getCurrentPosition(async position => {
+            const lat = position.coords.latitude;
+            const lon = position.coords.longitude;
+            const cxr = document.getElementById('cxr').value;
+            const tariff = document.getElementById('tariff').value;
+            const rule = document.getElementById('rule').value;
+
+            if (cxr == "" || tariff == "") {
+
+                console.log("insert carrier and tariff");
+
+                return;
+            }
+
+            const data = { lat, lon, cxr, tariff, rule };
+            const options = {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            };
+
+            const response = await fetch('/api', options);
+            const json = await response.json(data);
+
+            console.log(json);
+
+        });
+
+    } else {
+
+        console.log('geolocation not available');
+    }
+
+}
+
+
+async function getData() {
+
+    const cxr = document.getElementById('cxr').value;
+    const tariff = document.getElementById('tariff').value;
+    const rule = document.getElementById('rule').value;
+
+    const data = { cxr, tariff, rule };
+    const options = {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    };
+
+
+    if (data.length == 0) {
+
+        console.log("there is no data");
+
+    } else {
+
+        // we call first the api getGen to get the generic sequence (ONLY ONE value is expected)
+        const genUrl = `getGeneric/${cxr}/${tariff}`;
+        const genResponse = await fetch(genUrl);
+        const seq = await genResponse.json();
+
+        console.log(seq);
+
+        document.getElementById('generic').textContent = "First generic sequence: " + seq;
+
+        if (rule != "") {
+
+            //when a rule is input in the HTML we run the api getRules to look for the rule or rules
+
+            const rulesUrl = `getRules/${cxr}/${tariff}/${rule}`;
+            const ruleResponse = await fetch(rulesUrl);
+            const json = await ruleResponse.json();
+
+            console.log(json);
+
+            //first we remove the body element
+            var element = document.getElementById('body');
+            element.parentNode.removeChild(element);
+
+            // now we create it again
+
+            var tableRef = document.getElementById('myTable');
+
+            var tableBody = document.createElement('tbody');
+
+            tableBody.setAttribute('id', 'body');
+
+            tableRef.appendChild(tableBody);
+
+
+            for (var item of json) {
+
+                var newrow = tableBody.insertRow();
+
+                var newcell = newrow.insertCell(0);
+                var newtext = document.createTextNode(item.cxr);
+                newcell.appendChild(newtext);
+
+                newcell = newrow.insertCell(1);
+                newtext = document.createTextNode(item.tariff);
+                newcell.appendChild(newtext);
+
+                newcell = newrow.insertCell(2);
+                newtext = document.createTextNode(item.seq);
+                newcell.appendChild(newtext);
+
+                newcell = newrow.insertCell(3);
+                newtext = document.createTextNode(item.rule);
+                newcell.appendChild(newtext);
+
+                newcell = newrow.insertCell(4);
+                newtext = document.createTextNode(item.app);
+                newcell.appendChild(newtext);
+
+                newcell = newrow.insertCell(5);
+                newtext = document.createTextNode(new Date(item.date).toLocaleString());
+                newcell.appendChild(newtext);
+            }
+        }
+    }
+}
+
+// btnSubmit.addEventListener('click', sendData);
+btnFind.addEventListener('click', getData);
---
+++ server.js
@@ -0,0 +1,328 @@
+//@ts-check
+
+const express = require('express');
+const Datastore = require('nedb');
+const oracledb = require('oracledb');
+require('dotenv').config();
+
+const app = express();
+app.listen(3000, () => console.log('listening at 3000'));
+app.use(express.static('public'));
+app.use(express.json({ limit: '100kb' }));
+
+const database = new Datastore('database.db');
+
+database.loadDatabase();
+
+const con = {
+
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    connectString: process.env.DB_CON
+
+};
+
+app.get('/getGeneric/:cxr/:tariff', (request, response) => {
+
+    const cxr = request.params.cxr;
+
+    const tariff = request.params.tariff;
+
+    var sql = sequel(cxr, tariff, "");
+
+    oracledb.getConnection(con, function(err, connection) {
+
+        if (err) {
+            console.log(con);
+            console.error(err.message);
+            return;
+        }
+
+        connection.execute(sql, [], { maxRows: 1 }, function(err, result) {
+
+            if (err) {
+
+                console.error(err.message);
+                doRelease(connection);
+                return;
+            }
+
+            response.json(result.rows[0][2]);
+
+            doRelease(connection);
+
+        });
+
+        response.append(cxr);
+
+        return response;
+    });
+
+});
+
+app.get('/getRules/:cxr/:tariff/:rule', (request, response) => {
+
+    const cxr = request.params.cxr;
+    const tariff = request.params.tariff;
+    const rule = request.params.rule != "" ? request.params.rule : { $regex: /^[A-Z0-9]{4}/ };
+
+    var sql = sequel(cxr, tariff, rule);
+
+    oracledb.getConnection(con, function(err, connection) {
+
+        if (err) {
+            console.log(con);
+            console.error(err.message);
+            return;
+        }
+
+        connection.execute(sql, [], { maxRows: 0 }, function(err, result) {
+
+            if (err) {
+
+                console.error(err.message);
+                doRelease(connection);
+                return;
+            }
+
+            database.remove({}, { multi: true }, function(err, numRemoved) {
+                database.loadDatabase(function(err) {
+                    // done
+                });
+            });
+
+            database.loadDatabase();
+
+            for (var i = 0; i < result.rows.length; i++) {
+
+                const data = {};
+
+                var row = result.rows[i];
+                data.cxr = row[0];
+                data.tariff = row[1];
+                data.seq = row[2];
+                data.rule = row[3];
+                data.app = row[4];
+                data.date = row[5];
+
+                database.insert(data);
+            }
+
+            // database.find({}).sort({ rule: 1 }).skip(0).limit(0).exec(function(err, docs) {
+            //     // docs is [doc3, doc1]
+
+            //     response.json(docs);
+
+            // });
+
+            database.find({}).exec((err, docs) => {
+
+                let newDocs = docs.map((doc) => {
+
+                    doc.date = new Date(doc.date).valueOf(); // new property
+                    return doc; // return the new document
+
+                });
+
+                newDocs.sort((a, b) => {
+
+                    return b.date - a.date;
+                });
+
+                response.json(newDocs);
+
+            });
+
+            // database.find({}, (err, data) => {
+
+            //     if (err) {
+
+            //         response.end();
+            //         return;
+            //     }
+
+            //     response.json(data);
+            // });
+
+            doRelease(connection);
+
+        });
+    });
+
+});
+
+function doRelease(connection) {
+
+    connection.close(function(err) {
+
+        if (err) {
+
+            console.error(err.message);
+        }
+    });
+}
+
+
+function sequel(cxr, tariff, rule) {
+
+    var string;
+
+    var wildcard = " ";
+
+    if (rule != "") {
+
+        wildcard = "AND b.RULE IN (" + rule + ") ";
+    }
+
+    string = "SELECT  DISTINCT a.GOVERNINGCARRIER," +
+
+        "a.RULETARIFF, " +
+        "a.SEQNO, " +
+        "b.RULE, " +
+        "a.HIPMINFAREAPPL, " +
+        "TO_CHAR (a.CREATEDATE, 'MM-DD-YYYY HH24:MI') AS CREATEDATE " +
+        "FROM EXCLUDEBYRULE a " +
+        "LEFT OUTER JOIN EXCLBYRULERULE b ON a.CREATEDATE=b.CREATEDATE AND a.VENDOR=b.VENDOR AND a.GOVERNINGCARRIER=b.GOVERNINGCARRIER AND a.RULETARIFF=b.RULETARIFF " +
+        "LEFT OUTER JOIN EXCLBYRULEFARECLASS c ON a.CREATEDATE=c.CREATEDATE AND a.VENDOR=c.VENDOR AND a.GOVERNINGCARRIER=c.GOVERNINGCARRIER AND a.RULETARIFF=c.RULETARIFF " +
+        "LEFT OUTER JOIN EXCLBYRULEFARETYPE d ON a.CREATEDATE=d.CREATEDATE AND a.VENDOR=d.VENDOR AND a.GOVERNINGCARRIER=d.GOVERNINGCARRIER AND a.RULETARIFF=d.RULETARIFF " +
+        "WHERE    a.EXPIREDATE = TIMESTAMP '9999-12-31 23:59:59.999' " +
+        "AND      a.GOVERNINGCARRIER = '" + cxr + "' " +
+        "AND      a.RULETARIFF = " + tariff + " " +
+
+        wildcard +
+
+        // "AND      b.RULE IN (" + rule + ") " +
+        "AND      a.TARIFFCAT = -1 " +
+        "AND      a.ROUTINGTARIFF1 = -1 " +
+        "AND      a.ROUTINGTARIFF2 = -1 " +
+        "AND      a.RULETARIFFCODE = ' ' " +
+        "AND      a.USERAPPLTYPE = ' ' " +
+        "AND      a.USERAPPL = ' ' " +
+        "AND      a.GLOBALDIR = ' ' " +
+        "AND      a.MPMIND = 'N' " +
+        "AND      a.ROUTINGIND = 'N' " +
+        "AND      a.ROUTING = ' ' " +
+        "AND      a.HIPMINFAREAPPL = 'Y' " +
+        "AND      a.HIPFARECOMPAPPL = 'N' " +
+        "AND      a.HIPSAMEGROUPAPPL = 'N' " +
+        "AND      a.DMCMINFAREAPPL = 'N' " +
+        "AND      a.DMCFARECOMPAPPL = 'N' " +
+        "AND      a.DMCSAMEGROUPAPPL = 'N' " +
+        "AND      a.COMMINFAREAPPL = 'N' " +
+        "AND      a.COMFARECOMPAPPL = 'N' " +
+        "AND      a.COMSAMEGROUPAPPL = 'N' " +
+        "AND      a.CPMMINFAREAPPL = 'N' " +
+        "AND      a.CPMFARECOMPAPPL = 'N' " +
+        "AND      a.CPMSAMEGROUPAPPL = 'N' " +
+        "AND      a.COPMINFAREAPPL = 'N' " +
+        "AND      a.COPFARECOMPAPPL = 'N' " +
+        "AND      a.COPSAMEGROUPAPPL = 'N' " +
+        "AND      a.OSCFARECOMPAPPL = 'N' " +
+        "AND      a.OSCSAMEGROUPAPPL = 'N' " +
+        "AND      a.RSCFARECOMPAPPL = 'N' " +
+        "AND      a.RSCSAMEGROUPAPPL = 'N' " +
+        "AND      b.FOOTNOTE = ' ' " +
+        "AND      c.FARECLASS is NULL " +
+        "AND      d.FARETYPE is NULL " +
+        "GROUP BY a.GOVERNINGCARRIER, a.RULETARIFF, a.SEQNO, b.RULE, a.HIPMINFAREAPPL, a.CREATEDATE " +
+        "ORDER BY a.SEQNO";
+
+    return string;
+}
+
+// *******************************************************NOT UN USE **************************************************************
+
+// app.post('/api', (request, response) => {
+
+//     const data = request.body;
+//     const timestamp = Date.now();
+//     data.timestamp = timestamp;
+//     database.insert(data);
+//     response.json(data);
+// });
+
+
+
+// function searchRule(rq) {
+
+//     var cxr = rq.cxr;
+//     var tariff = rq.tariff;
+
+//     const con = {
+
+//         user: "sg208528",
+//         password: "mt9ZXvpGB6",
+//         // connectString: "(DESCRIPTION = (LOAD_BALANCE = ON) (ADDRESS_LIST=(ADDRESS = (PROTOCOL = TCP)(HOST = sachlp-cdc770)(PORT = 1521)) (ADDRESS = (PROTOCOL = TCP)(HOST = sachlp-tcc870)(PORT = 1521))) (CONNECT_DATA = (SERVICE_NAME = SACP_ADHOC)(SERVER = DEDICATED)))"
+//         connectString: "SACP_ADHOC"
+
+//     };
+
+//     const sql = `SELECT  DISTINCT a.GOVERNINGCARRIER, a.RULETARIFF, a.SEQNO FROM  EXCLUDEBYRULE a WHERE a.GOVERNINGCARRIER = :cxr AND RULETARIFF = :tariff`;
+
+//     oracledb.getConnection(con, function(err, connection) {
+
+//         if (err) {
+//             console.log(con);
+//             console.error(err.message);
+//             return;
+//         }
+
+//         console.log(cxr);
+//         console.log(tariff);
+//         console.log(sql);
+
+//         connection.execute(sql,
+
+//             {
+//                 cxr: { dir: oracledb.BIND_IN, val: cxr + " ", type: oracledb.STRING },
+
+//                 tariff: { dir: oracledb.BIND_IN, val: tariff }
+
+//             },
+
+//             {
+
+//                 maxRows: 1
+//             },
+
+//             function(err, result) {
+
+//                 if (err) {
+
+//                     console.error(err.message);
+//                     doRelease(connection);
+//                     return;
+//                 }
+//                 console.log(result.rows);
+//                 database.insert(result);
+//                 // rs = result;
+//                 doRelease(connection);
+//                 return result.rows;
+//             });
+//     });
+
+// }
+
+
+// app.post('/get', (request, response) => {
+
+//     const rq = request.body;
+//     const cxr = rq.cxr;
+//     const tariff = rq.tariff;
+//     const rule = rq.rule != "" ? rq.rule : { $regex: /^[A-Z0-9]{4}/ };
+
+//     response.json(searchRule(rq));
+
+//     console.log(response);
+
+//     database.find({ "cxr": cxr, "tariff": tariff, "rule": rule }, (err, data) => {
+
+//         if (err) {
+
+//             response.end();
+//             return;
+//         }
+
+//         response.json(data);
+//     });
+// });
